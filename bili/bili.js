02d62d24bbcc3edcd368e65d70b9bbc87cf74c1a
Arthur <unicrix@gmail.com>
2021-01-04 23:29:01 +0800

    fix a minor bug

--- bili/bili.js
+++ bili/bili.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
 const { program } = require("commander");
 
-program.command("d <url>", "Download video").command("login").parse(process.argv);
+program
+  .command("d <url>", "Download video")
+  .command("login", "Login Bilibili")
+  .parse(process.argv);
