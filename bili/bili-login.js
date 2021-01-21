f3e00fccbdd005c0b362fda27de5470f9547a126
Arthur <unicrix@gmail.com>
2021-01-21 22:04:18 +0800

    add auto login feature

--- bili/bili-login.js
+++ bili/bili-login.js
@@ -1,3 +1,63 @@
 #!/usr/bin/env node
 
-// TODO: Add login feature to capture cookie automatically
+const puppeteer = require("puppeteer-core");
+const fs = require("fs-extra");
+const path = require("path");
+
+(async () => {
+  const browser = await puppeteer.launch({
+      executablePath: process.env.CHROME_PATH || "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe",
+      headless: false,
+      args: ["--window-size=800,600"]
+  });
+  const page = await browser.newPage();
+  await page.goto("https://passport.bilibili.com/login");
+  const { username, password } = parseUsernamePassword();
+  if (!username || !password) {
+    console.error(
+      "Place username and password in automatic.login file to login!!"
+    );
+    process.exit(1);
+  }
+  await page.type("#login-username", username);
+
+  await page.type("#login-passwd", password);
+
+  await Promise.all([
+    page.click(".btn-box .btn-login"),
+    page.waitForNavigation({ waitUntil: "networkidle0", timeout: 0 }),
+  ]);
+  const cookie = await page.cookies();
+  writeCookie(cookie);
+  await browser.close();
+  console.log(`Welcome ${username}!`);
+})();
+
+function parseUsernamePassword() {
+  let keyStore = path.join(__dirname, "..", "automatic.login");
+
+  if (fs.existsSync(keyStore)) {
+    keyStore = fs.readFileSync(keyStore).toString();
+    const [username, password] = keyStore.split(/\r?\n/);
+    return {
+      username,
+      password,
+    };
+  }
+  return {};
+}
+
+function writeCookie(cookie) {
+  const cookieString = `"${cookie
+    .reduce((cookies, c) => {
+      const { name, value, domain } = c;
+      if (domain && domain.includes("bilibili.com")) {
+        cookies[name] = value;
+        cookies.push(`${name}=${value}`);
+      }
+      return cookies;
+    }, [])
+    .join("; ")}"`;
+
+  fs.writeFileSync(path.join(__dirname, "..", "cookie.txt"), cookieString);
+}
